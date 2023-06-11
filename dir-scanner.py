855ba2efa3c242ce2f053d8a2b9a0961b92ab117
py2krnl <84682551+py2krnl@users.noreply.github.com>
2023-06-12 01:34:33 +0300

    v0.01

--- dir-scanner.py
+++ dir-scanner.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
+import argparse
 import requests
 import os
 import threading
+from colorama import Fore
+import sys
 
-target_domain = str(input("Target domain > "))
-wordlist_path = str(input("Wordlist Path > "))
+
+version = 0.01
+
 headers = {
     "User-Agent" : "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
     "Sec-Ch-Ua": "AppleWebKit/605.1.15",
@@ -15,6 +20,9 @@ headers = {
     "Sec-Fetch-Dest": "document"
 }
 
+
+
+
 def check(domain: str, wordlist: str, header: dict):
     try:
         if os.path.exists(wordlist):
@@ -23,7 +31,7 @@ def check(domain: str, wordlist: str, header: dict):
                 threads = []
                 for data in _data:
                     data = data.rstrip()
-                    url = f"https://{domain}/{data}"
+                    url = f"{args.protocol}://{domain}/{data}"
                     t = threading.Thread(target=send_request, args=(url, header))
                     t.start()
                     threads.append(t)
@@ -35,11 +43,32 @@ def check(domain: str, wordlist: str, header: dict):
 
 def send_request(url: str, header: dict):
     try:
-        response = requests.get(url=url, headers=header , timeout=2.0)
-        if response.ok:
-            print(response.url)
+        response = requests.get(url=url, headers=header , timeout=5.0 , allow_redirects=False)
+        if response.status_code >= 200 and response.status_code <= 299:
+            print(f"[{Fore.GREEN}+{Fore.RESET}]  {response.url:<37} \t{Fore.GREEN}({Fore.RESET}STATUS:{response.status_code}|SIZE:{len(response.content)}{Fore.GREEN}){Fore.RESET}")
+        elif response.status_code >= 300 and response.status_code <= 399:
+            print(f"[{Fore.YELLOW}*{Fore.RESET}]  {response.url:<37} \t{Fore.YELLOW}({Fore.RESET}STATUS:{response.status_code}|SIZE:{len(response.content)}{Fore.YELLOW}){Fore.RESET}")
+        else:
+            pass
     except Exception as e:
         exit(e)
 
 if __name__ == "__main__":
-    check(domain=target_domain, wordlist=wordlist_path, header=headers)
+    parser = argparse.ArgumentParser(description="Directory Scanning Tool", usage=f"python {sys.argv[0]} --protocol http --target-domain example.com --wordlist-path wordlist.txt")
+    parser.add_argument("-p","--protocol", type=str, help="Protocol (http or https)")
+    parser.add_argument("-t","--target-domain", type=str, help="Target domain")
+    parser.add_argument("-w","--wordlist-path", type=str, help="Wordlist path")
+
+
+    if len(sys.argv) < 2:
+        parser.print_help()
+        sys.exit(1)
+
+
+    args = parser.parse_args()
+
+    os.system("clear")
+    print(f"\n* * * Target is: {args.target_domain} * * *\n")
+    print("")
+
+    check(domain=args.target_domain, wordlist=args.wordlist_path, header=headers)
